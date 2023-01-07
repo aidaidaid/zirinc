a2b355b9aeda611bc4085f7c7cc21825806516b9
AidaShirinova <aidash@wizardsdev.com>
2023-01-07 23:52:28 +0400

    done

---
+++ script.js
@@ -0,0 +1,26 @@
+const array = ['c', 'a', 't', ' ', 'p', 'e', 'n', ' ', 'a', 'p', 'p', 'l', 'e'];
+
+function reverseWords(arr) {
+    let start = 0;
+    let end = arr.length - 1;
+    reverse(arr, start, end);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === ' ') {
+            reverse(arr, start, i - 1);
+            start = i + 1;
+        }
+    }
+    reverse(arr, start, arr.length - 1);
+    return arr;
+}
+
+function reverse(arr, start, end) {
+    for (start; start < end; start++) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        end--;
+    }
+}
+
+console.log(reverseWords(array));
